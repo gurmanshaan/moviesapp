f7a56e599ff02df59cd96736dc49037d89fc3e2e
gurmanshaan <gurmanshaan@gmail.com>
2021-05-13 01:08:03 +0530

    completed main nav component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
+import { Container } from '@material-ui/core';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
+import SimpleBottomNavigation from './components/MainNav';
+import Movies from './Pages/Movies/Movies';
+import Search from './Pages/Search/Search';
+import Series from './Pages/Series/Series';
+import Trending from './Pages/Trending/Trending';
 
 function App() {
     return (
-        <div>
+        <BrowserRouter>
             <Header />
-        </div>
+            <div className="app">
+                <Container>
+                    <Switch>
+                        <Route path='/' component={Trending} exact />
+                        <Route path='/movies' component={Movies} />
+                        <Route path='/series' component={Series} />
+                        <Route path='/search' component={Search} />
+                    </Switch>
+                </Container>
+            </div>
+            <SimpleBottomNavigation />
+        </BrowserRouter>
     )
 }
 
