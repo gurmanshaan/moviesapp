b5861fe74c45a8c94859cd7cd6f85ff4279097e3
gurmanshaan <gurmanshaan@gmail.com>
2021-05-14 01:42:21 +0530

    movies component completed

--- src/Pages/Movies/Movies.js
+++ src/Pages/Movies/Movies.js
@@ -1,17 +1,22 @@
 import axios from "axios"
 import { useEffect, useState } from "react";
+import Genres from "../../components/Genres";
 import CustomPagination from "../../components/Pagination/CustomPagination";
 import SingleContent from "../../components/SingleContent/SingleContent";
+import useGenres from "../../hooks/useGenre";
 
 const Movies = () => {
 
     const [page, setPage] = useState(1);
     const [content, setContent] = useState([]);
     const [numOfPages, setNumOfPages] = useState();
+    const [selectedGenres, setselectedGenres] = useState([]);
+    const [genres, setGenres] = useState([]);
+    const genreforURL = useGenres(selectedGenres);
 
     const fetchMovies = async () => {
         const { data } = await axios.get(`
-        https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&page=${page}`)
+        https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&page=${page}&with_genres=${genreforURL}`)
 
         //console.log(data)
         setContent(data.results);
@@ -20,11 +25,20 @@ const Movies = () => {
 
     useEffect(() => {
         fetchMovies();
-    }, [page])
+        // eslint-disable-next-line
+    }, [page, genreforURL])
 
     return (
         <div>
             <span className="pageTitle">Movies</span>
+            <Genres
+                type="movie"
+                selectedGenres={selectedGenres}
+                setselectedGenres={setselectedGenres}
+                genres={genres}
+                setGenres={setGenres}
+                setPage={setPage}
+            />
             <div className="trending">
                 {content && content.map((c) =>
                     <SingleContent
