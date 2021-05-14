cf39f0bf4bce975dac2817d489900d7081201cc8
gurmanshaan <gurmanshaan@gmail.com>
2021-05-14 15:29:02 +0530

    completed modal component

--- src/components/SingleContent/SingleContent.js
+++ src/components/SingleContent/SingleContent.js
@@ -1,12 +1,13 @@
 import { Badge } from "@material-ui/core";
 import { img_300, unavailable } from "../../config/config";
+import ContentModal from "../ContentModal/ContentModal";
 import "./SingleContent.css";
 
 const SingleContent = ({
     id, poster, title, date, media_type, vote_average
 }) => {
     return (
-        <div className="media">
+        <ContentModal media_type={media_type} id={id}>
             <Badge badgeContent={vote_average} color={vote_average > 6 ? 'primary' : 'secondary'} />
             <img className="poster" src={poster ? `${img_300}/${poster}` : unavailable} alt={title} />
             <b className="title">{title}</b>
@@ -14,7 +15,7 @@ const SingleContent = ({
                 {media_type === 'tv' ? "TV Series" : "Movie"}
                 <span className="subTitle">{date}</span>
             </span>
-        </div>
+        </ContentModal>
     )
 }
 
